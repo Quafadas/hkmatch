e01ffc4e5fc06a428a65307c9972e035675b3c6e
Simon Parten <quafadas@gmail.com>
2025-08-10 17:57:53 +0200

    tidy

--- project.scala
+++ project.scala
@@ -8,8 +8,18 @@ import scala.quoted.*
 @main def call =
   println("x")
   import scala.quoted.*
-  val x1 = mac(TypeInferrer.Auto)
+  val origTest = original.TypeInferrer.FromTuple[Int]()
+
+  /** In user-space, you get a warning
+  * the type test for original.TypeInferrer.FromTuple[Int] cannot be checked at runtime because its type arguments can't be determined from original.TypeInferrer
+  *
+  * which isn't terribly descriptive...
+  */
+
+  val hmmm = origTest match
+    case TypeInferrer.Auto => println("yey") // simple case succcess - _should_ this work?
+    case TypeInferrer.FromTuple[Int]() => println("???")
+
+  // in macro land, this turns into a hard to track error.
   val x2 =
     mac(TypeInferrer.FromTuple[(Int, Int)]())
-  println(x1)
-  println(x2)
